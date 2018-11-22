78aee7757a78566e766f2e70bba989b9d2335368
Wheezy <mateusz.wedel@gmail.com>
2018-11-22 13:40:59 +0100

    Created server application #ES-10 Done

---
+++ estolowka-backend/src/main/java/pl/prodzajto/estolowkabackend/EstolowkaBackendApplication.java
@@ -0,0 +1,14 @@
+package pl.prodzajto.estolowkabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstolowkaBackendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EstolowkaBackendApplication.class, args);
+    }
+}
