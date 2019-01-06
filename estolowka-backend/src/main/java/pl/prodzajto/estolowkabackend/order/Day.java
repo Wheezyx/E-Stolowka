85426a736acaff718a64ef27e81e5bd214cebfdf
Paulina Mironiuk <p.mironiuk1@gmail.com>
2019-01-06 16:16:41 +0100

    added Day-Meal relation #ESW-27 TO VERIFY

--- estolowka-backend/src/main/java/pl/prodzajto/estolowkabackend/order/Day.java
+++ estolowka-backend/src/main/java/pl/prodzajto/estolowkabackend/order/Day.java
@@ -5,13 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
 
-import java.util.ArrayList;
+import java.util.Set;
 
 @Entity
 @Data
@@ -23,6 +25,7 @@ class Day {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private ArrayList<Meal> meals;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "day")
+    private Set<Meal> meals;
     private LocalDate selectedDay;
 }
