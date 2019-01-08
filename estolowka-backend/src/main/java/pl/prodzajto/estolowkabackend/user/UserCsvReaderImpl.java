de12cad09b57284347e07ba72c10196284bdd0a7
Mateusz Smagieł <mateusz.smagiel@gmail.com>
2019-01-08 22:38:47 +0100

    #ESW-29 Created UserCsvReader with simple implementation to save users from CSV file into database

---
+++ estolowka-backend/src/main/java/pl/prodzajto/estolowkabackend/user/UserCsvReaderImpl.java
@@ -0,0 +1,55 @@
+package pl.prodzajto.estolowkabackend.user;
+
+import com.opencsv.CSVReader;
+import org.apache.commons.text.CharacterPredicates;
+import org.apache.commons.text.RandomStringGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+@Service
+public class UserCsvReaderImpl implements UserCsvReader {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserCsvReaderImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public boolean readCSVFile(MultipartFile multipartFile) {
+        try (Reader inputStreamReader = new InputStreamReader(multipartFile.getInputStream());
+             CSVReader reader = new CSVReader(inputStreamReader)) {
+            String[] record;
+            while ((record = reader.readNext()) != null) {
+                userRepository.save(createUser(record));
+            }
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private UserEntity createUser(String[] record) {
+        UserEntity user = new UserEntity();
+        user.setEmail(record[0]);
+        user.setName(record[1]);
+        user.setSurname(record[2]);
+        user.setIndex(Integer.parseInt(record[3]));
+        user.setPassword(randomPassword());
+        return user;
+    }
+
+    private String randomPassword() {
+        RandomStringGenerator randomStringGenerator =
+                new RandomStringGenerator.Builder()
+                        .withinRange('0', 'z')
+                        .filteredBy(CharacterPredicates.LETTERS, CharacterPredicates.DIGITS)
+                        .build();
+        return randomStringGenerator.generate(8);
+    }
+}
