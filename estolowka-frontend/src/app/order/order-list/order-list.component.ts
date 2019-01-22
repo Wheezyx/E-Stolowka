e064f14243c4e31fa6c4ded4fd78b7548ec45ac3
Paulina Mironiuk <pmironiuk@soldevelo.com>
2019-01-22 17:30:42 +0100

    #ESW-34 fixed deleting orderables

--- estolowka-frontend/src/app/order/order-list/order-list.component.ts
+++ estolowka-frontend/src/app/order/order-list/order-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Day } from '../model/day';
 import { OrderService } from '../service/order.service';
+import { queueComponentIndexForCheck } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-order-list',
@@ -25,5 +26,10 @@ export class OrderListComponent {
   deleteDay(id: number) {
     let element = this.orders.find((i)=>i.id == id);
     this.orders.splice(this.orders.indexOf(element), 1);
+    let indexOfNextElementAfterDeletedOne = this.orders.indexOf(element) + 1;
+    for (let i = indexOfNextElementAfterDeletedOne; i < this.orders.length; i++) {
+      this.orders[i].id = i+1;
+    } 
   }
+  
 }
