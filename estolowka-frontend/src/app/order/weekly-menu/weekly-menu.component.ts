0fe2e92d0bd6a67b3e1567010ac8faa69e8881a2
Paulina Mironiuk <pmironiuk@soldevelo.com>
2019-03-13 22:06:50 +0100

    #ESW-43 used getMenuItems on click

--- estolowka-frontend/src/app/order/weekly-menu/weekly-menu.component.ts
+++ estolowka-frontend/src/app/order/weekly-menu/weekly-menu.component.ts
@@ -12,15 +12,6 @@ export class WeeklyMenuComponent implements OnInit {
   isEmpty: boolean = true;
 
   menuItems: WeeklyMenuItem[];
-  /*menuItems: WeeklyMenuItem[] = [
-    {date: "04/03/2019", breakfast: "Jajecznica z bekonem", dinner: "Spaghetti carbonara", supper: "Burger"},
-    {date: "05/03/2019", breakfast: "Naleśniki amerykańskie", dinner: "Placki ziemniaczane z gulaszem wołowym", supper: "Tortilla"},
-    {date: "06/03/2019", breakfast: "Owsianka", dinner: "Eskalopki", supper: "Tacosy"},
-    {date: "07/03/2019", breakfast: "Parówki, kiełbaski", dinner: "Pierogi ruskie, z mięsem", supper: "Burrito"},
-    {date: "08/03/2019", breakfast: "Tosty", dinner: "Łosoś grillowany", supper: "Krewetki"},
-    {date: "09/03/2019", breakfast: "Jajka na miękko", dinner: "Pizza Pepperoni", supper: "Naleśniki z twarogiem"},
-    {date: "10/03/2019", breakfast: "Croissant z dżemem", dinner: "Kotlet schabowy", supper: "Tatar"},
-  ]*/
 
   constructor(private menuService: MenuService) { }
 
