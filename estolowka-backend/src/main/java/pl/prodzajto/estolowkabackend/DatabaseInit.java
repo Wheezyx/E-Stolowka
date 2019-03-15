36a001d8144eaa1c44695621c735434be35a8170
Wheezy <mateusz.wedel@gmail.com>
2019-03-15 15:23:15 +0100

    Bug fixes.

--- estolowka-backend/src/main/java/pl/prodzajto/estolowkabackend/DatabaseInit.java
+++ estolowka-backend/src/main/java/pl/prodzajto/estolowkabackend/DatabaseInit.java
@@ -15,32 +15,36 @@ import java.util.Set;
 
 @AllArgsConstructor
 @Service
-public class DatabaseInit {
+public class DatabaseInit
+{
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final UserRoleRepository userRoleRepository;
-
+    
     @EventListener(ApplicationReadyEvent.class)
-    public void add() {
-        UserRole admin = userRoleRepository.findByRole("ADMIN");
-        UserRole userRole = userRoleRepository.findByRole("USER");
+    public void add()
+    {
+        UserRole adminRole = new UserRole(null, "ADMIN");
+        UserRole userRole = new UserRole(null, "USER");
+        userRoleRepository.save(adminRole);
+        userRoleRepository.save(userRole);
         Set<UserRole> roleSet1 = new HashSet<>();
         Set<UserRole> roleSet2 = new HashSet<>();
         roleSet1.add(userRole);
-        roleSet1.add(admin);
+        roleSet1.add(adminRole);
         roleSet2.add(userRole);
         UserEntity user = UserEntity.builder()
-                .index(212312)
-                .email("admin@gmail.com")
-                .roles(roleSet1)
-                .password(passwordEncoder.encode("admin"))
-                .build();
+                                    .index(212312)
+                                    .email("mateusz.wedel@gmail.com")
+                                    .roles(roleSet1)
+                                    .password(passwordEncoder.encode("admin"))
+                                    .build();
         UserEntity user2 = UserEntity.builder()
-                .index(123312)
-                .roles(roleSet2)
-                .email("user@gmail.com")
-                .password(passwordEncoder.encode("user"))
-                .build();
+                                     .index(123312)
+                                     .roles(roleSet2)
+                                     .email("user@gmail.com")
+                                     .password(passwordEncoder.encode("user"))
+                                     .build();
         userRepository.save(user);
         userRepository.save(user2);
     }
