b0aca64578343e2e82d2b91f58372269c5dfa8a1
Paulina Mironiuk <pmironiuk@soldevelo.com>
2019-03-22 12:37:18 +0100

    #ESW-53 deleted mocked data

--- estolowka-frontend/src/app/user/user.component.ts
+++ estolowka-frontend/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatSort } from '@angular/material';
 import { User } from './model/user';
 import { Pageable } from '../util/pageable';
 import { UserService } from './service/user.service';
@@ -11,28 +11,6 @@ import { merge } from "rxjs";
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  usersMock: User[] = [
-    {index: 123123, name: "Mateusz", surname: "Wedeł", email: "email@wp.pl", enabled: false},
-    {index: 123122, name: "Mateusz", surname: "Smagieł", email: "email@wp.pl", enabled: true},
-    {index: 223122, name: "Dawid", surname: "Majorczyk", email: "email@wp.pl", enabled: true},
-    {index: 113122, name: "Paulina", surname: "Mironiuk", email: "email@wp.pl", enabled: true},
-    {index: 145123, name: "Ola", surname: "Nowak", email: "email@wp.pl", enabled: true},
-    {index: 127622, name: "Kuba", surname: "Kowalski", email: "email@wp.pl", enabled: true},
-    {index: 223134, name: "Tomek", surname: "Nowak", email: "email@wp.pl", enabled: true},
-    {index: 145123, name: "Ola", surname: "Nowak", email: "email@wp.pl", enabled: true},
-    {index: 127622, name: "Kuba", surname: "Kowalski", email: "email@wp.pl", enabled: true},
-    {index: 223134, name: "Tomek", surname: "Nowak", email: "email@wp.pl", enabled: true},
-    {index: 123123, name: "Mateusz", surname: "Wedeł", email: "email@wp.pl", enabled: true},
-    {index: 123122, name: "Mateusz", surname: "Smagieł", email: "email@wp.pl", enabled: true},
-    {index: 223122, name: "Dawid", surname: "Majorczyk", email: "email@wp.pl", enabled: true},
-    {index: 113122, name: "Paulina", surname: "Mironiuk", email: "email@wp.pl", enabled: true},
-    {index: 145123, name: "Ola", surname: "Nowak", email: "email@wp.pl", enabled: true},
-    {index: 127622, name: "Kuba", surname: "Kowalski", email: "email@wp.pl", enabled: true},
-    {index: 223134, name: "Tomek", surname: "Nowak", email: "email@wp.pl", enabled: true},
-    {index: 145123, name: "Ola", surname: "Nowak", email: "email@wp.pl", enabled: true},
-    {index: 127622, name: "Kuba", surname: "Kowalski", email: "email@wp.pl", enabled: true},
-    {index: 223134, name: "Tomekaaa", surname: "Nowak", email: "email@wp.pl", enabled: true}
-  ]
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -41,13 +19,11 @@ export class UserComponent implements OnInit {
   pageSizeOptions = [10, 25, 50];
   totalUsers: number;
   displayedColumns: string[] = ['name', 'surname', 'index', 'email', 'active', 'block'];
-  //dataSource = new MatTableDataSource<User>(this.usersMock);
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.getUsers();
-    //this.dataSource.paginator = this.paginator;
     this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
     merge(this.sort.sortChange, this.paginator.page).subscribe(() => {
       this.pageable.page = this.paginator.pageIndex;
@@ -71,18 +47,7 @@ export class UserComponent implements OnInit {
       })
   }
 
-  blockMockedUser(user: User) {
-    user.enabled = false;
-    console.log("User blocked");
-  }
-
-  activateMockedUser(user: User) {
-    user.enabled = true;
-    console.log("User activated");
-  }
-
   private reload() {
     this.getUsers();
   }
-
 }
