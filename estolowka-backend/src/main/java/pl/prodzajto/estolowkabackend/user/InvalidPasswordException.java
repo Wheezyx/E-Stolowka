9d9a32fdcbcb02c1eac4b2904b4fb0a4a36a9cae
Wheezy <mateusz.wedel@gmail.com>
2019-06-03 15:57:43 +0200

    Check and force CI/CD.

--- estolowka-backend/src/main/java/pl/prodzajto/estolowkabackend/user/InvalidPasswordException.java
+++ estolowka-backend/src/main/java/pl/prodzajto/estolowkabackend/user/InvalidPasswordException.java
@@ -3,6 +3,6 @@ package pl.prodzajto.estolowkabackend.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE, reason = "Old password is incorrect")
+@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE, reason = "Old password is incorrect!")
 class InvalidPasswordException extends RuntimeException {
 }
